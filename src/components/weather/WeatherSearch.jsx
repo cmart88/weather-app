f9ea7581f81103fa1ea96e565fffe04236a22aac
Cmart88 <ftballplaya1221@gmail.com>
2022-04-11 18:33:03 -0700

    weather search

--- src/components/weather/WeatherSearch.jsx
+++ src/components/weather/WeatherSearch.jsx
@@ -1,16 +1,25 @@
+import { useState, useContext } from "react";
+import GithubContext from "../../context/github/GithubContext";
+import AlertContext from "../../context/alert/AlertContext";
+import { searchUsers } from "../../context/github/GithubActions";
 
-  const handleChange = (e) => setText(e.target.value);
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (text === "") {
-      setAlert("Please enter something", "error");
-    } else {
-      dispatch({ type: "SET_LOADING" });
-      const users = await searchUsers(text);
-      dispatch({ type: "GET_USERS", payload: users });
-      setText("");
-    }
-  };
+function UserSearch() {
+  //   const [text, setText] = useState("");
+  //   const { users, dispatch } = useContext(GithubContext);
+  //   const { setAlert } = useContext(AlertContext);
+
+  //   const handleChange = (e) => setText(e.target.value);
+  //   const handleSubmit = async (e) => {
+  //     e.preventDefault();
+  //     if (text === "") {
+  //       setAlert("Please enter something", "error");
+  //     } else {
+  //       dispatch({ type: "SET_LOADING" });
+  //       const users = await searchUsers(text);
+  //       dispatch({ type: "GET_USERS", payload: users });
+  //       setText("");
+  //     }
+  //   };
 
   return (
     <div className='grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8'>
